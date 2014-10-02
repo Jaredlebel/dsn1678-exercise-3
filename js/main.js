709456216d46c6bcccd45a4d06332b95cfd60c28
Jared Lebel <jared.lebel87@gmail.com>
2014-10-02 10:21:24 -0400

    Added data-states

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ var article = $('.sales');
 var aside = $('.sales__details');
 
 var buttonClickHandler = function () {
-    var currentState = sales__details.attr('data-state');
+    var currentState = aside.attr('data-state');
     
     if (currentState == 'active') {
-        sales__details.attr('data-state', 'inactive');
+        aside.attr('data-state', 'inactive');
     } else {
-        sales__details.attr('data-state', 'active'); 
+        aside.attr('data-state', 'active'); 
     }
 };
 
-theButton.on('click', buttonClickHandler);
+article.on('click', buttonClickHandler);
